1268a5935626898050ac7ac69008e90cc5d2d97c
yourlastword69 <151046862+yourlastword69@users.noreply.github.com>
2024-04-04 19:38:25 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDzENkWTUt6F4lFOWw-juSODJARBLkmTYo",
+    authDomain: "kwitter-13cba.firebaseapp.com",
+    databaseURL: "https://kwitter-13cba-default-rtdb.firebaseio.com",
+    projectId: "kwitter-13cba",
+    storageBucket: "kwitter-13cba.appspot.com",
+    messagingSenderId: "206184339372",
+    appId: "1:206184339372:web:75593882ee3c6882d6cc09"
+  };
